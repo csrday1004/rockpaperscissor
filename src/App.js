b38d4d4ce07b54927d3662f48b17200409f48379
JSR <csrday1004@gmail.com>
2024-03-20 12:38:22 +0900

    가위바위보 최종

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./App.css";
 import Box from "./component/Box";
 import Button from "./component/Button";
@@ -18,48 +18,51 @@ function App() {
     "https://feelstory.com/gnu/nori/img/3_on.png",
   ];
 
-  const [myPick, setMyPick] = useState(null);
-  const [comPick, setComPick] = useState(null);
+  const [myPick, setMyPick] = useState();
+  const [comPick, setComPick] = useState();
   const [myResult, setMyResult] = useState("?");
   const [comResult, setComResult] = useState("?");
+  const [count, setCount] = useState(0);
+  const [record, setRecord] = useState([0, 0, 0]);
+  // 승,무,패
 
-  const 승부결정 = () => {
+  const 승부결정 = (i) => {
     const 승부결정도우미 = [0, 1, 2, 0];
-
-    if (myPick === comPick) {
+    
+    // 내가 고른 거 저장
+    setMyPick(i);
+    // 컴퓨터 랜덤 픽 저장
+    const computerPick = Math.floor((Math.random() *data.length));
+    setComPick(computerPick);
+    // 클릭 수 저장
+    setCount(count + 1);
+    
+    let copy = [...record];
+    if (i === computerPick) {
       setMyResult("Tie");
       setComResult("Tie");
+      copy[1]++;
+    } else if (승부결정도우미[i + 1] === computerPick) {
+      setMyResult("Lose");
+      setComResult("Win");
+      copy[2]++;
     } else {
-      if (승부결정도우미[myPick + 1] === comPick) {
-        setMyResult("Lose");
-        setComResult("Win");
-      } else {
-        setMyResult("Win");
-        setComResult("Lose");
-      }
+      setMyResult("Win");
+      setComResult("Lose");
+      copy[0]++;
     }
+    setRecord(copy);
   };
 
-  useEffect(() => {
-    console.log("나", myPick);
-    console.log("컴", comPick);
-
-    if (myPick===null) {
-      return
-    }else{
-      승부결정()
-    }
-  }, [myPick, comPick]);
-
   return (
-    <div className="App" style={{ padding: 20 }}>
-      <h1 style={{ textAlign: "center" }}>가위바위보 게임</h1>
+    <div className="App" style={{ padding: 20, textAlign: "center"}}>
+      <h1>가위바위보 게임</h1>
       <div
         className="결투장"
         style={{ display: "flex", justifyContent: "center" }}
       >
-        <Box data={data[myPick]} myResult={myResult} who={"나"} />
-        <Box data={data[comPick]} comResult={comResult} who={"컴퓨터"} />
+        <Box data={data[myPick]} result={myResult} who={"나"} />
+        <Box data={data[comPick]} result={comResult} who={"컴퓨터"} />
       </div>
       <div
         className="선택버튼"
@@ -69,17 +72,32 @@ function App() {
           return (
             <div
               onClick={() => {
-                // 내가 고른 거 저장
-                setMyPick(i);
-                // 컴퓨터 랜덤 픽 저장
-                setComPick(Math.floor((Math.random() * 10) % 3));
+                승부결정(i);
               }}
+              key={i}
             >
-              <Button img={e} key={i} />
+              <Button img={e} />
             </div>
           );
         })}
       </div>
+      <div className="전적">
+        <p>
+          {count}경기 중 {record[0]}승 {record[1]}무 {record[2]}패
+        </p>
+        <p>
+          승률 :{" "}
+          {record[0] + record[2]
+            ? ((record[0] / (record[0] + record[2])) * 100).toFixed(1)
+            : 0}
+          %
+        </p>
+      </div>
+
+      {/* 리셋 */}
+      <div onClick={()=>{
+        window.location.reload()
+      }} style={{margin:'auto',padding:'10px', background:'black', width:'300px', borderRadius:'10px', color:'white', cursor:'pointer'}}>Reset</div>
     </div>
   );
 }
--- src/component/Box.js
+++ src/component/Box.js
@@ -1,11 +1,28 @@
 import React from 'react'
 
-const Box = ({data,who,myResult,comResult}) => {
+const Box = ({data,who,result}) => {
+
+  const resultStyle = () =>{
+    switch(result){
+      case 'Win':
+        return { background: 'blue' };
+
+      case 'Lose':
+        return { background: 'red', opacity:0.3};
+
+      case 'Tie':
+        return { background: 'green' };
+      default:
+        return {};
+    }
+    
+  }
+
   return (
-    <div className='box'>
+    <div className='box' style={resultStyle()}>
       <h1>{who}</h1>
       <img src={data?data:'https://cdna.artstation.com/p/assets/images/images/061/938/632/original/derend-marvel-ezgif-com-gif-maker-1.gif?1681968838'} alt='가위바위보'/>
-      <h2>{who==='나'?myResult:comResult}</h2>
+      <h2>{result}</h2>
     </div>
   )
 }
